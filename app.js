d1670ae66f75d974009cc7b5b0e4d8a713f072da
Anjan Rai <rai.unknown@gmail.com>
2020-11-22 14:34:33 +1100

    connection string for mongodb atlas added

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const { auth } = require('./middleware/auth')
 const { RegisterUser, LoginUser, LogoutUser, getUserDetails } = require('./controllers/authController');
-const url ='mongodb://localhost:27017/studyapp'
+// const url ='mongodb://localhost:27017/studyapp'
+const url = 'mongodb+srv://anjanraiz:anjanraiz@cluster0.l82k5.mongodb.net/revvedAI?retryWrites=true&w=majority'
 
-//OTP mobile verification: 16/11/2020
-// const app = require('express')()
-//   , http = require('http')
-//   , server = http.createServer(app)
-//   , io = require('socket.io').listen(server)
-//   , nStore = require('nstore')
-//   , client = require('twilio')(process.env.account_sid, process.env.auth_token)
-//   , speakeasy = require('speakeasy');
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://anjanraiz:<password>@cluster0.l82k5.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -43,90 +44,14 @@ const client = require('twilio')(accountSid, authToken);
 // .then((message) => console.log(message.sid));
 // +19107884144
 
-// var ouser= nStore.new('data/users.db',function(){
-//     console.log("Loader users.db")
-// })
-
-// app.get('/',function(req,res){
-//     res.render('index.jade')
-// });
-
-// function createUser(phone_number, code, socket) {
-//     ouser.save(phone_number, {code: code, verified: false}, function (saverr) {
-//       if (saverr) { throw saverr; }
-//       client.sendSms({
-//           to: phone_number,
-//           from: process.env.twilio_number,
-//           body: 'Your verification code is: ' + code
-//       }, function(twilioerr, responseData) {
-//         if (twilioerr) { 
-//           ouser.remove(phone_number, function(remerr) {if (remerr) { throw remerr; }});
-//           socket.emit('update', {message: "Invalid phone number!"});
-//         } else {
-//           socket.emit('code_generated');
-//         }
-//       });
-//     });
-//   }
-
-
-//   function checkVerified(socket, verified, number) {
-//     if (verified == true) {
-//       socket.emit('reset');
-//       socket.emit('update', {message: "You have already verified " + number + "!"});
-//       return true;
-//     }
-//     return false;
-//   }
-  
-
-//   io.sockets.on('connection', function(socket) {
-//     console.log('socket.io connected');
-//     socket.on('register', function(data) {
-//       var code = speakeasy.totp({key: 'abc123'});
-//       ouser.get(data.phone_number, function (geterr, doc, key) {
-//         if (geterr) {
-//           createUser(data.phone_number, code, socket);
-//         }
-//         else if (checkVerified(socket, doc.verified, data.phone_number) == false) {
-//           socket.emit('update', {message: "You have already requested a verification code for that number!"});
-//           socket.emit('code_generated');
-//         }
-//       });
-  
-//     });
-
-
-//     socket.on('verify', function(data) {
-//         ouser.get(data.phone_number, function (geterr, doc, key) {
-//           if (geterr) {
-//             socket.emit('reset');
-//             socket.emit('update', {message: "You have not requested a verification code for " + data.phone_number + " yet!"});
-//           }
-//           else if (checkVerified(socket, doc.verified, data.phone_number) == false && doc.code == parseInt(data.code)) {
-//             socket.emit('verified');
-//             socket.emit('update', {message: "You have successfully verified " + data.phone_number + "!"});
-//             users.save(data.phone_number, {code: parseInt(data.code), verified: true}, function (saverr) { if (saverr) { throw saverr; }});
-//           }
-//           else {
-//             socket.emit('update', {message: "Invalid verification code!"});
-//           }
-//         });
-
-//     })
-// })
-//Notification modules
-// const webpush=require('web-push');
-// const bodyParser =require('body-parser');
-// const path=require('path');
 const app =express()
 
-// mongoose.connect(url,{useNewUrlParser:true})
-// const con = mongoose.connection
+mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true})
+const con = mongoose.connection
 
-// con.on('open',()=>{
-//     console.log("Connected")
-// })
+con.on('open',()=>{
+    console.log("Connected")
+})
 
 app.use(express.json())
 
@@ -138,10 +63,10 @@ const authRouter = require('./routes/authentication');
 app.use('/auth', authRouter);
 
 const studentRouter =require('./routes/student')
-app.use('/student',studentRouter)
+app.use('/student', studentRouter)
 
 const eventRouter =require('./routes/event');
-const { UserInstance } = require('twilio/lib/rest/chat/v1/service/user');
+// const { UserInstance } = require('twilio/lib/rest/chat/v1/service/user');
 app.use('/event',eventRouter)
 
 app.listen(8000, ()=>{
