061cb8773a0a46cba45c0f26d258a781f4c4229c
Anjan Rai <rai.unknown@gmail.com>
2020-12-21 06:59:34 +1100

    updated port to use 80 in server, and atlas mongodb url

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const User = require('./models/user');
 const { auth } = require('./middleware/auth')
 const { RegisterUser, LoginUser, LogoutUser, getUserDetails } = require('./controllers/authController');
 // const url ='mongodb://localhost:27017/studyapp'
-const url = 'mongodb+srv://anjanraiz:anjanraiz@cluster0.l82k5.mongodb.net/revvedAI?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URL || 'mongodb+srv://anjanraiz:anjanraiz@cluster0.l82k5.mongodb.net/revvedAI?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 80;
 
 
 // this is to create a new service in twilio
@@ -73,51 +73,7 @@ passport.use(
         }
     )
 );
-// passport.use('facebook', new FacebookStrategy({
-//     clientID        : process.env.FACEBOOK_APP_ID,
-//     clientSecret    : process.env.FACEBOOK_APP_SECRET,
-//     callbackURL     : process.env.FACEBOOK_CALLBACK_URL
-//   },
-   
-//     // facebook will send back the tokens and profile
-//     function(access_token, refresh_token, profile, done) {
-//       // asynchronous
-//       process.nextTick(function() {
-//           console.log(profile);
-//         // find the user in the database based on their facebook id
-//         User.findOne({ 'id' : profile.id }, function(err, user) {
-   
-//           // if there is an error, stop everything and return that
-//           // ie an error connecting to the database
-//           if (err)
-//             return done(err);
-   
-//             // if the user is found, then log them in
-//             if (user) {
-//               return done(null, user); // user found, return that user
-//             } else {
-//               // if there is no user found with that facebook id, create them
-//               var newUser = new User();
-   
-//               // set all of the facebook information in our user model
-//               newUser.fb.id    = profile.id; // set the users facebook id                 
-//               newUser.fb.accessToken = access_token; // we will save the token that facebook provides to the user                    
-//               newUser.fb.firstName  = profile.name.givenName;
-//               newUser.fb.lastName = profile.name.familyName; // look at the passport user profile to see how names are returned
-//               newUser.fb.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-   
-//               // save our user to the database
-//               newUser.save(function(err) {
-//                 if (err)
-//                   throw err;
-   
-//                 // if successful, return the new user
-//                 return done(null, newUser);
-//               });
-//            } 
-//         });
-//       });
-//   }));
+
 app.use(passport.initialize());
 const userRouter = require('./routes/user');
 app.use('/user', userRouter);
