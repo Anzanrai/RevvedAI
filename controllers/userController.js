c19d36dc4531f85092266e6bd6688d7b00b01d55
Anjan Rai <rai.unknown@gmail.com>
2021-01-10 20:59:41 +1100

    changed import statement for User in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const {User} = require('../models/user');
 const {successResponse, errorResponse} = require('../middleware/responseFormat');
 const { Teacher, Guardian } = require('../models/user');
 // const { json } = require('body-parser');
